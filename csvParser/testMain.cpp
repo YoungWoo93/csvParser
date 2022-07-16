51423dc33556acd9d5c5a4dce321de62a449d84d
leeyongwoo93 <rmstjd23@gmail.com>
2022-07-16 22:30:31 +0900

    main 정리

--- csvParser/testMain.cpp
+++ csvParser/testMain.cpp
@@ -22,7 +22,6 @@ int main()
 	CSV inter1, inter2, inter3;
 	inter1.read("testFile/test1.csv");
 	inter2.read("testFile/test2.csv");
-	inter3.read("testFile/test3.csv");
 	for (auto it : inter1)
 	{
 		for (auto item : it)
@@ -40,6 +39,7 @@ int main()
 		cout << endl;
 	}
 	cout << endl;
+
 	inter2.eraseColumn(1);
 	for (int i = 0; i < inter2.size(); i++)
 	{
@@ -47,6 +47,7 @@ int main()
 		cout << endl;
 	}
 	cout << endl;
+
 	inter2.eraseColumn(inter2.size() - 1);
 	for (int i = 0; i < inter2.size(); i++)
 	{
@@ -54,32 +55,6 @@ int main()
 		cout << endl;
 	}
 	cout << endl;
-
-	while (true)
-	{
-		string testKey = "test";
-		int width = 1;
-		int height = 1;
-
-		for (int i = 0; i < 1000; i++)
-		{
-			inter3.addKey(testKey + to_string(i));
-			width++;
-			inter3.addColumn(vector<string>(width, "test"));
-			height++;
-		}
-
-		for (int i = 0; i < 1000; i++)
-		{
-			inter3.eraseKey(testKey + to_string(i));
-			width--;
-			inter3.eraseColumn(1);
-			height--;
-		}
-	}
-
-
-	//inter2.eraseKey("???");
 
 	inter1.write("testFile/test1-copy.csv");
 	inter2.write("testFile/test2-copy.csv");
